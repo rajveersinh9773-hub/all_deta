d9de184b2b41a5b5612d617fd60829364c47fef3
rajveersinh jadeja <rajveersinh9773@gmail.com>
2026-01-06 17:03:55 +0530

    Add files via upload

--- dsafinal/one.cpp
+++ dsafinal/one.cpp
@@ -45,6 +45,39 @@ void update(queue<int>& q) {
     else
         cout << "invalid index" << endl;
 }
+void bubblesort(queue<int>& q) {
+    if (q.empty()) {
+        cout << "queue is empty" << endl;
+        return;
+    }
+    int n = q.size();
+    for (int i = 0; i < n; i++) {
+        queue<int> temp;
+        int prev = q.front();
+        q.pop();
+        for (int j = 1; j < n - i; j++) {
+            int curr = q.front();
+            q.pop();
+
+            if (prev > curr) {
+                temp.push(curr);
+            } else {
+                temp.push(prev);
+                prev = curr;
+            }
+        }
+        temp.push(prev);
+        while (!q.empty()) {
+            temp.push(q.front());
+            q.pop();
+        }
+
+        q = temp;
+    }
+
+    cout << "queue sorted using bubble sort" << endl;
+}
+
 
 int main() {
     cout << "welcome to crud operations" << endl;
@@ -57,7 +90,8 @@ int main() {
         cout << "2. dequeue" << endl;
         cout << "3. display" << endl;
         cout << "4. update" << endl;
-        cout << "5. exit" << endl;
+        cout << "5. bubble sort" << endl;
+        cout << "6. exit" << endl;
         cout << "enter choice: ";
         cin >> choice;
 
@@ -84,8 +118,10 @@ int main() {
         case 4:
             update(q);
             break;
-
-        case 5:
+         case 5:
+            bubblesort(q);
+            break;
+        case 6:
             return 0;
 
         default:
